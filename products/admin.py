0bc0f6f40456d2ca6d6db7b54326e7c16c66722a
Manakoff <manakoooov@gmail.com>
2026-06-04 17:12:37 +0200

    Добавил подтверждение emails через Яндекс и сделал код чистым

--- products/admin.py
+++ products/admin.py
@@ -1,19 +1,21 @@
 from django.contrib import admin
 
-from products.models import ProductCategory, Product, Basket
+from products.models import Basket, Product, ProductCategory
 
 admin.site.register(ProductCategory)
 
+
 @admin.register(Product)
 class ProductAdmin(admin.ModelAdmin):
-    list_display = ('name', 'category', 'price', 'quantity')
-    fields = ('name', 'description', 'category', ('price', 'quantity'), 'image')
-    readonly_fields = ('description',)
-    search_fields = ('name',)
-    ordering = ('name',)
+    list_display = ("name", "category", "price", "quantity")
+    fields = ("name", "description", "category", ("price", "quantity"), "image")
+    readonly_fields = ("description",)
+    search_fields = ("name",)
+    ordering = ("name",)
+
 
 class BasketAdmin(admin.TabularInline):
     model = Basket
-    fields = ('product', 'quantity', 'created_timestamp')
-    readonly_fields = ('created_timestamp',)
+    fields = ("product", "quantity", "created_timestamp")
+    readonly_fields = ("created_timestamp",)
     extra = 0
--- products/models.py
+++ products/models.py
@@ -1,7 +1,7 @@
-from django.contrib.auth.models import AbstractUser
 from django.db import models
 
 from users.models import User
+
 
 class ProductCategory(models.Model):
     name = models.CharField(max_length=128, unique=True)
@@ -9,25 +9,26 @@ class ProductCategory(models.Model):
 
     def __str__(self):
         return self.name
-    
+
     class Meta:
-        verbose_name='категорию'
-        verbose_name_plural = 'Категории'
+        verbose_name = "категорию"
+        verbose_name_plural = "Категории"
+
 
 class Product(models.Model):
     name = models.CharField(max_length=256, unique=True)
     description = models.TextField(null=True, blank=True)
     price = models.DecimalField(max_digits=10, decimal_places=2)
     quantity = models.PositiveIntegerField(default=0)
-    image = models.ImageField(upload_to='products_images')
+    image = models.ImageField(upload_to="products_images")
     category = models.ForeignKey(to=ProductCategory, on_delete=models.PROTECT)
 
     def __str__(self):
-        return f'Продукт: {self.name} | Категория: {self.category.name}'
-    
+        return f"Продукт: {self.name} | Категория: {self.category.name}"
+
     class Meta:
-        verbose_name='Продукт'
-        verbose_name_plural = 'Продукты'
+        verbose_name = "Продукт"
+        verbose_name_plural = "Продукты"
 
 
 class BasketQuerySet(models.QuerySet):
@@ -37,17 +38,16 @@ class BasketQuerySet(models.QuerySet):
     def total_quantity(self):
         return sum(basket.quantity for basket in self)
 
+
 class Basket(models.Model):
     user = models.ForeignKey(to=User, on_delete=models.CASCADE)
     product = models.ForeignKey(to=Product, on_delete=models.CASCADE)
     quantity = models.PositiveIntegerField(default=0)
     created_timestamp = models.DateTimeField(auto_now_add=True)
-
     objects = BasketQuerySet.as_manager()
-    
+
     def __str__(self):
-        return f'Корзина для {self.user.username} | Продукт: {self.product.name}'
+        return f"Корзина для {self.user.username} | Продукт: {self.product.name}"
 
     def sum(self):
         return self.quantity * self.product.price
-
